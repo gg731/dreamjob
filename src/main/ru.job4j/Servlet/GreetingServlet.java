04f59f7631a2a6412c56c2558759652fbb1ee87c
137799 <g1377@protonmail.com>
2020-08-04 23:46:48 +0300

    JSON формат

--- src/main/ru.job4j/Servlet/GreetingServlet.java
+++ src/main/ru.job4j/Servlet/GreetingServlet.java
@@ -13,9 +13,10 @@ public class GreetingServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/plain");
+
         resp.setCharacterEncoding("UTF-8");
-        String name = req.getParameter("name");
+        String name = req.getParameter("text");
+        System.out.println(name);
         PrintWriter writer = new PrintWriter(resp.getOutputStream());
         writer.println("Nice to meet you, " + name);
         writer.flush();
